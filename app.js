ed2d91cb56329ae3cb129434b27da20b0d459aae
Adam Borowski <fl.borovsky@gmail.com>
2023-02-15 18:11:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
+app.get('/login', (req, res) => {
+  res.set('Location', 'http://localhost:3000/relogin');
+  res.status(302).end();
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 

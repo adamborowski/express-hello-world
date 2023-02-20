ac32b2a6d9f666b95838387387cd45cc9a7f22fc
Adam Borowski <fl.borovsky@gmail.com>
2023-02-20 23:09:53 +0100

    use post

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.json());
+app.use(express.urlencoded());
 app.get('/', (req, res) => res.type('html').send(html(req.query.redirect)));
 app.post('/login', (req, res) => {
     console.log(req.body)
